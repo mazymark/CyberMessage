82cfab28f8fec5f47282f8bf14d45e3b643ba58b
mazymark <120261931+mazymark@users.noreply.github.com>
2024-03-15 13:58:13 +0900

    404-page-update

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
 
 export default function PageNotFound({ currentUser, setCurrentUser }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 4000);
+  }, []);
   return (
     <>
-      <div>
-        <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser} />
-        <h1 className="posts-header">Page Not Found</h1>
-        <h1>404 - Page Not Found, redirect</h1>
-      </div>
+      <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <h1 className="posts-header">404 - Page Not Found</h1>
+      <p className="not-found">
+        We apologize, but the page you are looking for cannot be found. It seems
+        the page you're attempting to access does not exist or has been
+        relocated.
+        <br />
+        <br />
+        You will be redirected to the homepage shortly.
+      </p>
       <Footer />
     </>
   );
